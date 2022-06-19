79b03959f6fe57a0f617bff65db3c731af89dfb8
Edwin Top <edwin.okonkwo.extern@gmail.com>
2022-06-20 01:21:28 +0200

    ERC721 implements metadata.

--- contracts/BNFT.sol
+++ contracts/BNFT.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BNFT is ERC721Enumerable, Ownable {
+contract BNFT is ERC721, Ownable {
     constructor() ERC721("Braintrust NFT", "BNFT") {}
 }
